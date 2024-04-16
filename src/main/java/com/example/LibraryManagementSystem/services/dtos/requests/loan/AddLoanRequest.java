8ac6dcf568e91aaec4100cdd786021cd08526356
Canfer Ulaş Şahin <ulssahn@outlook.com>
2024-04-16 16:19:05 +0300

    Project

---
+++ src/main/java/com/example/LibraryManagementSystem/services/dtos/requests/loan/AddLoanRequest.java
@@ -0,0 +1,13 @@
+package com.example.LibraryManagementSystem.services.dtos.requests.loan;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddLoanRequest {
+}
---
+++ src/main/java/com/example/LibraryManagementSystem/services/mappers/BookMapper.java
@@ -0,0 +1,7 @@
+package com.example.LibraryManagementSystem.services.mappers;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BookMapper {
+}
